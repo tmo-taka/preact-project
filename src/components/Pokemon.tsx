c5ba5c7b1f52778f1fdcdd3385c17cc6a87aa3b3
takahara tomoya <t.tkorock221@gmail.com>
2023-11-02 14:52:25 +0900

    エラー対応

--- src/components/Pokemon.tsx
+++ src/components/Pokemon.tsx
@@ -6,6 +6,7 @@ const Pokemon: preact.FunctionComponent = () => {
     console.log('読み込まれた')
     const id = useRecoilValue(randomIdState)
     const [pokeImg, setPokeImg] = useState('');
+    const [error, setError] = useState(false);
 
     useEffect(() => {
         const fetchPoke = async() => {
@@ -13,22 +14,19 @@ const Pokemon: preact.FunctionComponent = () => {
                 const apiUrl = `/pokeApi/${id}`;
                 // throwする
                 const data =  await (await fetch(apiUrl)).json();
-                if( !data ) {throw 'error'}
+                if( !data ) {throw new Error('error')}
                 const { sprites } = data;
                 const url = sprites.front_default;
                 await setPokeImg(url);
                 console.log(pokeImg);
-            } catch(e:unknown) {
-                console.log(e);
-                throw (e);
+            } catch(e) {
+                setError(true);
             }
         }
-        try {
-            fetchPoke();
-        } catch (e){
-            throw new Error(e);
-        }
+        fetchPoke();
     },[])
+
+    if(error) {throw new Error('エラーです')}
 
     return (
         <div>
