4282644e78c9f6f27728279346b5d3025af95446
takahara tomoya <t.tkorock221@gmail.com>
2023-11-10 09:51:38 +0900

    Content-Locationに該当する修正

--- src/hooks/useTextToSpeak.ts
+++ src/hooks/useTextToSpeak.ts
@@ -1,4 +1,4 @@
-const options = {
+const postOptions = {
     method: 'POST',
     headers: {
         accept: 'text/event-stream',
@@ -9,18 +9,38 @@ const options = {
     },
     body: JSON.stringify({
         text: 'Hello',
-        voice: 's3://voice-cloning-zero-shot/d9ff78ba-d016-47f6-b0ef-dd630f59414e/female-cs/manifest.json',
+        voice: 's3://mockingbird-prod/abigail_vo_6661b91f-4012-44e3-ad12-589fbdee9948/voices/speaker/manifest.json',
         output_format: 'mp3',
         voice_engine: 'PlayHT2.0'
     })
 };
 
-export const useTextToSpeak = (text:string) => {
+const getOptions = {
+    method: 'GET',
+    headers: {
+        accept: 'text/event-stream',
+        AUTHORIZATION: import.meta.env.VITE_PLAYHT_API_SECRET_KEY,
+        'X-USER-ID': import.meta.env.VITE_PLAYHT_API_USER_ID
+
+    },
+};
+
+export const useTextToSpeak = async(text:string) => {
     try {
-        fetch('/playhtApi', options)
-            .then(response => response.json())
-            .then(response => console.log(response))
-    } catch(e) {
-        console.log(e);
+        const res = await fetch('/playhtApi', postOptions);
+        // 自動でContent-LocationでリダイレクトするとproxyできないのでこちらでGET処理をする
+        let url = await res.headers.get('Content-Location');
+        if(url) {
+            url = url.replace('https://api.play.ht/api/v2/tts','/playhtApi')
+            const resp = await fetch(url, getOptions);
+            console.log(resp);
+        }
+    } catch (e) {
+        console.log(e)
     }
+        // .then(response => {
+        //     if(response.headers.get('Content-Location') != null) {
+        //         url = response.headers.get('Content-Location');
+        //     }
+        // })
 }
