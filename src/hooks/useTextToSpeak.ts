6520cc3d6ac6dbbdad314a62b5e8797d948ce9b0
takahara tomoya <t.tkorock221@gmail.com>
2023-11-11 15:33:06 +0900

    mp3のURLは取得できた

--- src/hooks/useTextToSpeak.ts
+++ src/hooks/useTextToSpeak.ts
@@ -8,39 +8,36 @@ const postOptions = {
 
     },
     body: JSON.stringify({
-        text: 'Hello',
+        text: 'Hello I am money.',
         voice: 's3://mockingbird-prod/abigail_vo_6661b91f-4012-44e3-ad12-589fbdee9948/voices/speaker/manifest.json',
         output_format: 'mp3',
         voice_engine: 'PlayHT2.0'
     })
 };
 
-const getOptions = {
-    method: 'GET',
-    headers: {
-        accept: 'text/event-stream',
-        AUTHORIZATION: import.meta.env.VITE_PLAYHT_API_SECRET_KEY,
-        'X-USER-ID': import.meta.env.VITE_PLAYHT_API_USER_ID
-
-    },
-};
-
 export const useTextToSpeak = async(text:string) => {
     try {
         const res = await fetch('/playhtApi', postOptions);
-        // 自動でContent-LocationでリダイレクトするとproxyできないのでこちらでGET処理をする
-        let url = await res.headers.get('Content-Location');
-        if(url) {
-            url = url.replace('https://api.play.ht/api/v2/tts','/playhtApi')
-            const resp = await fetch(url, getOptions);
-            console.log(resp);
+        const decoder = new TextDecoder();
+
+        if(res.body) {
+            const streamRead = res.body.getReader();
+            const read = async () => {
+                const { done, value } = await streamRead.read();
+                if (done) {
+                    return;
+                }
+                const decodeData: string = decoder.decode(value);
+                if(decodeData.includes('completed')){
+                    const stringifyData = JSON.stringify(decodeData);
+                    const url = stringifyData.substring(stringifyData.indexOf('https'), (stringifyData.indexOf('.mp3') + 4))
+                    console.log(url)
+                }
+                return read();
+            };
+            await read();
         }
     } catch (e) {
         console.log(e)
     }
-        // .then(response => {
-        //     if(response.headers.get('Content-Location') != null) {
-        //         url = response.headers.get('Content-Location');
-        //     }
-        // })
 }
